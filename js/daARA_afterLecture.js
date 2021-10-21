66e6297babbef234c0148de7bdf480f01e1922e7
BUZZINGPolarBear <jjh63360@kau.kr>
2021-10-22 02:43:26 +0900

    Add Canvs Algorithm

--- js/daARA_afterLecture.js
+++ js/daARA_afterLecture.js
@@ -1,10 +1,8 @@
-const { ServerlessApplicationRepository } = require("aws-sdk");
-
 function drawBarPlot()
 {
   var canvas = document.getElementById("EmotionResultBarGraphCavas");
   canvas.width = 900;
-  canvas.height = 500;
+  canvas.height = 600;
   var ctx = canvas.getContext("2d");
   
   var total_emotion_cnt = 0;
@@ -27,62 +25,53 @@ function drawBarPlot()
   for(var i=0; i<sorted_TEST_final_emotion.length; i++) total_emotion_cnt += TEST_final_emotion_result[sorted_TEST_final_emotion[i]];
   console.log(total_emotion_cnt);
 
-  ctx.fillStyle = '#E43176';
-  ctx.fillRect(canvas.width/2 - 450, canvas.height-250, 45, -(TEST_final_emotion_result[sorted_TEST_final_emotion[0]]/total_emotion_cnt)*500);
-  ctx.beginPath();
-  ctx.arc(canvas.width/2, canvas.height-470, 7, 0, Math.PI*2);
-  ctx.fillStyle = '#E43176';
-  ctx.fill();
-  ctx.fillStyle = 'white';
-  ctx.font = '20px arial';
-  ctx.fillText(sorted_TEST_final_emotion[0], canvas.width/2 + 15, canvas.height-465);
-  ctx.fillText(Math.floor((TEST_final_emotion_result[sorted_TEST_final_emotion[0]]/total_emotion_cnt)*100) + "%", canvas.width/2 - 445, canvas.height-220);
-  ctx.closePath();
+  draw_canvar(TEST_final_emotion_result);
+}
 
-  ctx.fillStyle = '#FA4660';
-  ctx.fillRect(canvas.width/2 - 400, canvas.height-250, 45, -(TEST_final_emotion_result[sorted_TEST_final_emotion[1]]/total_emotion_cnt)*500);
-  ctx.beginPath();
-  ctx.arc(canvas.width/2, canvas.height-420, 7, 0, Math.PI*2);
-  ctx.fillStyle = '#FA4660';
-  ctx.fill();
-  ctx.fillStyle = 'white';
-  ctx.font = '20px arial';
-  ctx.fillText(sorted_TEST_final_emotion[1], canvas.width/2 + 15, canvas.height-415);
-  ctx.fillText(Math.floor((TEST_final_emotion_result[sorted_TEST_final_emotion[1]]/total_emotion_cnt)*100) + "%", canvas.width/2 - 395, canvas.height-220);
-  ctx.closePath();
+var color = ["#E43176", "#FA4660", "#FF6447", "#FF852C", "#FFA600"];
+var widthSize = [450, 400, 350, 300, 250];
+var heightSize = [430, 390, 350, 310, 270];
+function draw_canvar(final_emotion_result) {
+  var canvas = document.getElementById("EmotionResultBarGraphCavas");
+  canvas.width = 900;
+  canvas.height = 500;
+  var ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = "#FF6447";
-  ctx.fillRect(canvas.width/2 - 350, canvas.height-250, 45, -(TEST_final_emotion_result[sorted_TEST_final_emotion[2]]/total_emotion_cnt)*500);
-  ctx.beginPath();
-  ctx.arc(canvas.width/2, canvas.height-370, 7, 0, Math.PI*2);
-  ctx.fill();
-  ctx.fillStyle = 'white';
-  ctx.font = '20px arial';
-  ctx.fillText(sorted_TEST_final_emotion[2], canvas.width/2 + 15, canvas.height-365);
-  ctx.fillText(Math.floor((TEST_final_emotion_result[sorted_TEST_final_emotion[2]]/total_emotion_cnt)*100) + "%", canvas.width/2 - 345, canvas.height-220);
-  ctx.closePath();
-  
-  ctx.fillStyle = '#FF852C';
-  ctx.fillRect(canvas.width/2 - 300, canvas.height-250, 45, -(TEST_final_emotion_result[sorted_TEST_final_emotion[3]]/total_emotion_cnt)*500);
-  ctx.beginPath();
-  ctx.arc(canvas.width/2, canvas.height-320, 7, 0, Math.PI*2);
-  ctx.fill();
-  ctx.fillStyle = 'white';
-  ctx.font = '20px arial';
-  ctx.fillText(sorted_TEST_final_emotion[3], canvas.width/2 + 15, canvas.height-315);
-  ctx.fillText(Math.floor((TEST_final_emotion_result[sorted_TEST_final_emotion[3]]/total_emotion_cnt)*100) + "%", canvas.width/2 - 295, canvas.height-220);
-  ctx.closePath();
+  var total_emotion_cnt = 0;
 
-  ctx.fillStyle = '#FFA600';
-  ctx.fillRect(canvas.width/2 - 250, canvas.height-250, 45, -(TEST_final_emotion_result[sorted_TEST_final_emotion[4]]/total_emotion_cnt)*500);
-  ctx.beginPath();
-  ctx.arc(canvas.width/2, canvas.height-270, 7, 0, Math.PI*2);
-  ctx.fill();
-  ctx.fillStyle = 'white';
-  ctx.font = '20px arial';
-  ctx.fillText(sorted_TEST_final_emotion[1], canvas.width/2 + 15, canvas.height-265);
-  ctx.fillText(Math.floor((TEST_final_emotion_result[sorted_TEST_final_emotion[4]]/total_emotion_cnt)*100) + "%", canvas.width/2 - 245, canvas.height-220);
-  ctx.closePath();
+  var sorted_final_emotion = Object.keys(final_emotion_result).sort(
+    function (a, b) {
+      return final_emotion_result[b] - final_emotion_result[a];
+    }
+  );
 
+  for (var i = 0; i < sorted_final_emotion.length; i++)
+    total_emotion_cnt += final_emotion_result[sorted_final_emotion[i]];
+  console.log(total_emotion_cnt);
 
+  for (step = 0; step < 5; step++) 
+  {
+    ctx.fillStyle = color[step];
+    ctx.fillRect(
+      canvas.width / 2 - widthSize[step],
+      canvas.height - 250,
+      45, -(final_emotion_result[sorted_final_emotion[step]] / total_emotion_cnt) * 500
+    );
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2 - 100, canvas.height - heightSize[step],  7,  0,  Math.PI * 2);
+    ctx.fillStyle = color[step];
+    ctx.fill();
+    ctx.fillStyle = "white";
+    ctx.font = "20px arial";
+    ctx.fillText(
+      sorted_final_emotion[step],
+      canvas.width / 2 - 85,
+      canvas.height - heightSize[step]+5
+    );
+    if(Math.floor((final_emotion_result[sorted_final_emotion[step]] / total_emotion_cnt) * 100)>=10)
+        ctx.fillText(Math.floor((final_emotion_result[sorted_final_emotion[step]] / total_emotion_cnt) * 100) + "%", canvas.width / 2 - widthSize[step] + 5, canvas.height-220);
+    else
+        ctx.fillText(Math.floor((final_emotion_result[sorted_final_emotion[step]] / total_emotion_cnt) * 100) + "%", canvas.width / 2 - widthSize[step] + 12, canvas.height-220);
+    ctx.closePath();
+  }
 }
